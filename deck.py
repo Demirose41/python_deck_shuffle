df074bd5a6af926b10e3c69c3ce649b2e5e174d3
demirose41 <demirose41@gmail.com>
2021-07-26 15:34:39 -0700

    4. Added __repr__ for Deck that returns the # of remaining cards in the form of a phrase

--- deck.py
+++ deck.py
@@ -5,6 +5,9 @@ class Deck :
         suits = ['Hearts','Diamonds','Clubs','Spades']
         values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']  
         self.cards = [ Card(value,suit) for suit in suits for value in values]
+    def __repr__(self):
+        return f'Deck of {self.count()} cards'
+
     def count(self):
         return len(self.cards)
 
@@ -12,5 +15,6 @@ class Deck :
 
 deck = Deck()
 print(deck.count())
+print(deck)
 
 
