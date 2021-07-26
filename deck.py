62d387c9d6d9a4c0114da1a336dc5731dc1faffc
demirose41 <demirose41@gmail.com>
2021-07-26 15:31:49 -0700

    3. Added count method to Deck class that return total remaining cards in deck

--- deck.py
+++ deck.py
@@ -5,12 +5,12 @@ class Deck :
         suits = ['Hearts','Diamonds','Clubs','Spades']
         values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']  
         self.cards = [ Card(value,suit) for suit in suits for value in values]
-        
-
+    def count(self):
+        return len(self.cards)
 
 
 
 deck = Deck()
-print(deck.cards)
+print(deck.count())
 
 
